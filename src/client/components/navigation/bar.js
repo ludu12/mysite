cfef80954e2781e870baf178a84648166ab30842
Luke Dunscombe <ldunscombe@leantechniques.com>
2020-01-24 08:43:41 -0600

    removing icon as it seems to break build

--- src/client/components/navigation/bar.js
+++ src/client/components/navigation/bar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import GithubIcon from '@material-ui/icons/Github';
 import './navigation.scss';
 import IconButton from '@material-ui/core/IconButton';
 
@@ -15,14 +13,14 @@ const Bar = () => {
         <div className='bar'>
           <div>
             <Typography>Luke Dunscombe</Typography>
-            
+
           </div>
           <div>
             <IconButton href='https://twitter.com/LukeDunscombe'>
-              <TwitterIcon/>
+              <p>Twitter</p>
             </IconButton>
             <IconButton href='https://github.com/ludu12'>
-              <GithubIcon/>
+              <p>Github</p>
             </IconButton>
           </div>
         </div>
