04cdaae9d78cea8bbb8075f0392dcf0c72167447
Luke Dunscombe <ldunscombe@leantechniques.com>
2020-01-29 18:51:14 -0600

    Changing name to server.js for heroku

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -3,8 +3,7 @@ const config = require('./webpack.config');
 
 config.mode = 'production';
 config.plugins.push(new webpack.DefinePlugin({
-  API_BASE: JSON.stringify('https://harmony-api.azurewebsites.net'),
-  OKTA_CLIENT_ID: JSON.stringify('0oao8p5v0dZ9Zbbbc356')
+  API_BASE: JSON.stringify('https://lukedunscombe.herokuapp.com'),
 }));
 
 module.exports = config;
