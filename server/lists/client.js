109b5a560303317a344504242b9fee2fcb4480d8
Luke Dunscombe <dunscombe1@gmail.com>
2020-04-06 09:41:19 -0500

    Removing server from this repo

--- server/lists/client.js
+++
@@ -1,9 +0,0 @@
-import { Text } from '@keystonejs/fields';
-
-export const ClientSchema = {
-  schemaDoc: 'Client information',
-  fields: {
-    name: { type: Text, isRequired: true, isUnique: true },
-    location: { type: Text },
-  },
-};
--- server/lists/tech-stack.js
+++
@@ -1,8 +0,0 @@
-import { Text } from '@keystonejs/fields';
-
-export const TechStackSchema = {
-  schemaDoc: 'Tech stacks categories',
-  fields: {
-    name: { type: Text, isRequired: true, isUnique: true },
-  },
-};
